de46d3e1846584269f30de0abc788f01beb407ae
ramgsuri <ramgsuri@gmail.com>
2021-04-15 00:56:29 +0530

    [Ram]  Modifies  Expressions

--- src/lectures/part1/LearnExpressions.scala
+++ src/lectures/part1/LearnExpressions.scala
@@ -4,8 +4,15 @@ object LearnExpressions extends App{
 
   // Instructions  Vs Expressions
   // In Imperative Languages like Java you tell the Computer to do something via Instructions
-  // Whereas in Declarative Language you pass the expression / Value
+  // Whereas in Scala you pass expressions & expressions are evaluated
   val condition: Boolean = true
   val answer = if(condition) 5 else 0
   print(answer)  // 5
+
+  // Example of Code Block
+  val aCodeBlock = {
+    val firstVariable: Int = 10
+    val condition: Boolean = true
+    val result = if(condition) "hello" else "bye"
+  }
 }
