29a931ec603b034b5f35d9158dd4dc56479bc431
ramgsuri <ramgsuri@gmail.com>
2021-04-15 00:28:37 +0530

    [Ram]  Adds First very Elementary class for scala rocks workshop to demonstrate variable types

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# Scala Rocks Workshop
+This Repository is for folks who want to learn scala from beginners level 
+to intermediate.
+
+ 
---
+++ src/lectures/part1/ValuesVariablesTypes.scala
@@ -0,0 +1,16 @@
+package lectures.part1
+
+object ValuesVariablesTypes extends App{
+
+  val x: Int = 42
+  print(x)
+  // x = 23 Vals are Immutable cannot change value of x
+  // Compiler can infer types val x = 42
+
+  val aChar: Char = 'a'
+  val aBoolean: Boolean = true
+  val aShort: Short = 1
+
+
+
+}
