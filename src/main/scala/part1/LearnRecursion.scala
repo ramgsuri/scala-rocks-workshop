fdb7348b66b23e3f4fe314310c7bb79a85cf58f8
ramgsuri <ramgsuri@rediffmail.com>
2021-04-29 00:13:41 +0530

    [Ram] adds another approach using accumulator for recursion

--- src/main/scala/part1/LearnRecursion.scala
+++ src/main/scala/part1/LearnRecursion.scala
@@ -12,5 +12,29 @@ object LearnRecursion extends App{
     }
   }
 
-  factorial(5)
+ // print(" Factorial is " + factorial(6))
+  /** Output is  */
+  // Computing factorial of 5I first need factorial of 4
+  // Computing factorial of 4I first need factorial of 3
+  // Computing factorial of 3I first need factorial of 2
+  // Computing factorial of 2I first need factorial of 1
+  // Computed factorial of 2
+  // Computed factorial of 3
+  // Computed factorial of 4
+  // Computed factorial of 5
+
+  // Solve using accumulator
+
+  def anotherFacrtorial(n: Int): BigInt = {
+    def factHelper(x: Int, accumulator: BigInt): BigInt = {
+      if(x <=1) accumulator
+      else factHelper(x-1, x*accumulator)
+    }
+    factHelper(n,1)
+  }
+
+  print(anotherFacrtorial(5))
+
+
+
 }
